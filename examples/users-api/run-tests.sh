d35b84dc1103c5cd3b07238c0c93da6bc3ef5cf4
Kim Andre Furevikstrand <lozymon@gmail.com>
2026-03-28 18:40:16 -0300

    Simplify run-tests.sh: remove variable for docker compose command

--- examples/users-api/run-tests.sh
+++ examples/users-api/run-tests.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-COMPOSE="docker compose -f docker-compose.yml"
-
 # Start the stack and wait for all health checks to pass.
-$COMPOSE up --build -d --wait
+docker compose -f docker-compose.yml up --build -d --wait
 
 # Run the crosscheck test suite.
 ../../cx run tests/ --env-file .env
